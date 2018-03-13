23781cb4e4dd449024ee554f2b8855ad7a6fd1bf
Stylianos Andreolas <deathlupuss@yahoo.gr>
2018-03-13 20:43:01 +0200

    CameraController #2 Attempt

--- Assets/Stelios/Scripts/CameraController.cs
+++ Assets/Stelios/Scripts/CameraController.cs
@@ -9,63 +9,111 @@ public class CameraController : MonoBehaviour {
     public Vector3 offset = new Vector3(0f, 18f, 15.5f);
 
     public Vector3 panLimit = new Vector3(5f,5f,5f);
-    public float panSpeed = 10f;
+    public float panSpeed;
     public float panBorderThickness = 10f;
-    public Vector3 pos;
-    private bool isFollowingTarget;
+
+    public Vector3 MovingCameraPosition;
+
+    public bool isFollowingTarget;
+
+    public bool isReturning;
+    private Vector3 StartReturningPos;
+    private float returningTimer;
 
 
     void Start()
     {
-        pos = target.position + offset;
+        transform.position = target.position + offset;
+        isFollowingTarget = true;
     }
 
     // Update is called once per frame
     void Update() {
 
-        //if (isFollowingTarget)
-        //{
-        //    pos = target.position + offset;
-        //}
-        //else
-        //{
-
-        //}
-
-        //Vector3 pos = transform.position;
-
-        //get camera position relative to target
-
-
-        pos = transform.position;
-
-        if (Input.mousePosition.y >= Screen.height - panBorderThickness)
+        if (isFollowingTarget && !isReturning)
         {
-            pos.z += panSpeed * Time.deltaTime; //pan camera up
-        }
-        else if (Input.mousePosition.y <= panBorderThickness)
-        {
-            pos.z -= panSpeed * Time.deltaTime; //pan camera down
-        }
-        else if (Input.mousePosition.x >= Screen.width - panBorderThickness)
-        {
-            pos.x += panSpeed * Time.deltaTime; //pan camera right
-        }
-        else if (Input.mousePosition.x <= panBorderThickness)
-        {
-            pos.x -= panSpeed * Time.deltaTime; //pan camera left
+            transform.position = target.position + offset;
         }
 
-        else if (Input.GetKey(KeyCode.W)|| Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
-         {
-            pos = target.position + offset;
+
+        if (!isReturning)
+        {
+            if (Input.mousePosition.y >= Screen.height - panBorderThickness)
+            {
+                isFollowingTarget = false;
+                MovingCameraPosition = new Vector3(transform.position.x,
+                                                transform.position.y,
+                                                transform.position.z - panSpeed * Time.deltaTime);
+            }
+            else if (Input.mousePosition.y <= panBorderThickness)
+            {
+                isFollowingTarget = false;
+                MovingCameraPosition = new Vector3(transform.position.x,
+                                                transform.position.y,
+                                                transform.position.z + panSpeed * Time.deltaTime);
+            }
+            else if (Input.mousePosition.x <= panBorderThickness)
+            {
+                isFollowingTarget = false;
+                MovingCameraPosition = new Vector3(transform.position.x + panSpeed * Time.deltaTime,
+                                                transform.position.y,
+                                                transform.position.z);
+            }
+            else if (Input.mousePosition.x >= Screen.width + panBorderThickness)
+            {
+                isFollowingTarget = false;
+                MovingCameraPosition = new Vector3(transform.position.x - panSpeed * Time.deltaTime,
+                                                transform.position.y,
+                                                transform.position.z);
+            }
+
+            MovingCameraPosition.Set(Mathf.Clamp(MovingCameraPosition.x, target.position.x + offset.x - panLimit.x, target.position.x + offset.x + panLimit.x),
+                    MovingCameraPosition.y,
+                    Mathf.Clamp(MovingCameraPosition.z, target.position.z + offset.z - panLimit.z, target.position.z + offset.z + panLimit.z));
+
+            if (isFollowingTarget == false)
+            {
+                transform.position = MovingCameraPosition; 
+            }
         }
+       
 
-        pos.Set(Mathf.Clamp(pos.x, target.position.x - panLimit.x, target.position.x + panLimit.x),
-                pos.y,
-                Mathf.Clamp(pos.z, target.position.z - panLimit.z, target.position.z + panLimit.z));
+        if (isReturning || (Input.GetKeyDown(KeyCode.Space) && isFollowingTarget == false))
+        {
+            //transform.position = target.position + offset; 
+            //isFollowingTarget = true;
 
-        transform.position = pos; //set new camera position value
-    }
+            if (transform.position != target.position + offset)
+            {
+                if (!isReturning)
+                {
+                    isFollowingTarget = true;
+                    isReturning = true;
+                    StartReturningPos = transform.position;
+                    returningTimer = 0;
+                }
+
+                transform.position = Vector3.Lerp(StartReturningPos, target.position + offset, returningTimer);
+                returningTimer += Time.deltaTime / 0.5f;
+            }
+            else
+            {
+                isReturning = false;
+            }
+        }   
         
+        if((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || 
+           Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D)) && transform.position != target.position + offset) 
+        {
+            if (!isReturning)
+            {
+                isFollowingTarget = true;
+                isReturning = true;
+                StartReturningPos = transform.position;
+                returningTimer = 0;
+            }
+        }
+        
+    }
+    
 }
