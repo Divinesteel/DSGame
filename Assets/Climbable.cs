e00e97e082d4b42b15fc434e286c6777c632aea0
Stylianos Andreolas <deathlupuss@yahoo.gr>
2018-04-06 20:39:35 +0300

    Player Climb/ Climbable Box Pref

---
+++ Assets/Climbable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Climbable : MonoBehaviour {
+
+    public Transform endPosition;
+    public float height;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerMovement>().SetJumpDestination(endPosition,height);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerMovement>().SetJumpDestination(null,0);
+        }
+    }
+}
