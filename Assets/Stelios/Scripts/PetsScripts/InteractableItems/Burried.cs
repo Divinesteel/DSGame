72957edd648ebddd8faf35d40a45a96510bb8da2
Stylianos Andreolas <deathlupuss@yahoo.gr>
2018-04-24 15:07:45 +0300

    Dust + Barel (Animations - Triggers)

---
+++ Assets/Stelios/Scripts/PetsScripts/InteractableItems/Burried.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Burried : MonoBehaviour {
+
+    private bool hasBeingDigged;
+    
+    public bool isPetDigging;
+    public GroundPetInteract groundPetInteract;
+
+    [Header("Dust Particle System")]
+    public ParticleSystem DustParticleSystem;
+	// Use this for initialization
+	protected void Start () {
+
+	}
+
+    // Update is called once per frame
+    protected void Update () {
+
+        if(groundPetInteract != null)
+        {
+            isPetDigging = groundPetInteract.IsDigging;
+            if(isPetDigging == true && !hasBeingDigged)
+            {
+                DustParticleSystem.Play();
+            }
+            else
+            {
+                DustParticleSystem.Stop();
+            }
+        }
+
+	}
+
+    protected void SetDigStatus(bool a)
+    {
+        hasBeingDigged = a;
+    }
+
+    protected bool GetHasBeenDigged()
+    {
+        return hasBeingDigged;
+    }
+
+    virtual protected void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "GroundPet")
+        {
+            groundPetInteract = other.GetComponent<GroundPetInteract>();
+
+        }
+    }
+
+    virtual protected void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "GroundPet")
+        {
+            groundPetInteract = null;
+            isPetDigging = false;
+        }
+    }
+}
---
+++ Assets/Stelios/Scripts/PetsScripts/InteractableItems/BurriedBarrel.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurriedBarrel : Burried {
+
+    public GameObject barel;
+
+    private PlayerInteract playerInteract;
+    private Animation barelAnim;
+
+    private bool hasDigAnimationStarted;
+    private bool hasDigAnimationPaused;
+
+    private bool hasBeenThrownToRiver;
+
+    // Use this for initialization
+    void Start() {
+        base.Start();
+        barelAnim = barel.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        base.Update();
+
+        if (base.GetHasBeenDigged() == false)
+        {
+            if (groundPetInteract != null)
+            {
+                if (isPetDigging == true)
+                {
+                    if (!hasDigAnimationStarted)
+                    {
+                        barelAnim.Play();
+                        hasDigAnimationStarted = true;
+                    }
+
+                    if (hasDigAnimationPaused)
+                    {
+                        barelAnim.enabled = true;
+                        hasDigAnimationPaused = false;
+                    }
+
+                }
+                else
+                {
+                    barelAnim.enabled = false;
+                    hasDigAnimationPaused = true;
+                }
+
+            }
+
+            if (isPetDigging && !barelAnim.IsPlaying("Animation_BurriedBarel"))
+            {
+                base.SetDigStatus(true);
+            }
+        }
+        else
+        {
+
+            if (!hasBeenThrownToRiver && playerInteract.isInteracting) 
+            {
+                barelAnim.Play("Animation_BarelRiver");
+                hasBeenThrownToRiver = true;
+            }
+        }
+    }
+
+    override protected void OnTriggerEnter(Collider other)
+    {
+        base.OnTriggerEnter(other);
+        if (other.gameObject.tag == "Player")
+        {
+            playerInteract = other.gameObject.GetComponent<PlayerInteract>();
+        }
+    }
+
+    protected override void OnTriggerExit(Collider other)
+    {
+        base.OnTriggerExit(other);
+        if(other.gameObject.tag == "Player")
+        {
+            playerInteract = null;
+        }
+    }
+
+}
