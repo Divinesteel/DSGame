87c80df3241ea38e4ceae242f875c712148b8881
JasonGrigor <grigoropoulosj@gmail.com>
2018-10-14 21:39:03 +0300

    Boulder Animation Interact Script (incomplete)

---
+++ Assets/BoulderTrigger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderTrigger : MonoBehaviour {
+
+    private PlayerInteract playerInteract;
+    private Animator boulderAnim;
+
+    public bool isboulderRolling;
+
+    // Use this for initialization
+    void Start () {
+
+        boulderAnim = GetComponent<Animator>();
+        isboulderRolling = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (playerInteract != null)
+        {
+            if (isboulderRolling)
+            { 
+                boulderAnim.SetTrigger("Boulder Trigger");
+            }
+        }
+    }
+
+    protected void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerInteract = other.gameObject.GetComponent<PlayerInteract>();
+
+        }
+    }
+
+    protected void OnPlayerInteract()
+    {
+        isboulderRolling = true;
+    }
+
+}
