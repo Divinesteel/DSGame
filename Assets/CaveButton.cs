f7ff8c650c7e049477a2f18bce96a9170c473a33
JasonGrigor <grigoropoulosj@gmail.com>
2018-12-23 19:01:44 +0200

    Cave Buttons

---
+++ Assets/CaveButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveButton : MonoBehaviour {
+
+    public bool isButtonPressed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "GroundPet")
+        {
+            isButtonPressed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "GroundPet")
+        {
+            isButtonPressed = false;
+        }
+    }
+
+    public bool GetButtonStatus()
+    {
+        return isButtonPressed;
+    }
+}
---
+++ Assets/CaveMechanism.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveMechanism : MonoBehaviour
+{
+
+    public bool button1Pressed;
+    public bool button2Pressed;
+
+    public CaveButton button1;
+    public CaveButton button2;
+
+    public Animator anim;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateButtonsStatuses();
+        if (button1Pressed && button2Pressed)
+        {
+            anim.SetBool("ButtonsArePressed", true); //Sets Animator "ButtonsArePressed" To True
+        }
+    }
+
+    void UpdateButtonsStatuses()
+    {
+        button1Pressed = button1.GetButtonStatus();
+        button2Pressed = button2.GetButtonStatus();
+    }
+
+}
