b569fba0324483ec1a37ee380bc6dd0258c8a6b8
Stylianos Andreolas <35779815+Divinesteel@users.noreply.github.com>
2019-05-05 15:09:25 +0300

    Drag Crystal

--- Assets/Stelios/Scripts/DragAndDrop/Drag.cs
+++ Assets/Stelios/Scripts/DragAndDrop/Drag.cs
@@ -8,6 +8,8 @@ public class Drag : MonoBehaviour {
     public DragAndDrop dragAndDropSystem;
 
     private Text text;
+    private Image image;
+
 	// Use this for initialization
 	void Start () {
         text = gameObject.GetComponent<Text>();
@@ -15,18 +17,32 @@ public class Drag : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (image != null)
+        {
+            image.transform.position = Input.mousePosition;
+        }
 	}
 
     public void SetDNDName()
     {
         dragAndDropSystem.SetDraggedItem(text.text);
+        if (text != null && Input.GetKey(InputManager.IM.orderGroundPet))
+        {
+            //GetComponentInParent<Image>().sprite = ...;
+            image = GetComponentInParent<Image>();
+            image = Instantiate(image,this.gameObject.transform);
+        }
     }
 
     public void RemoveDNDName()
     {
         dragAndDropSystem.DropItem();
         dragAndDropSystem.RemoveDraggedItem();
-
+        Destroy(image.gameObject);
+        image = null;
+        //if (text != null)
+        //{
+        //    GetComponentInParent<Image>().sprite = sprite;
+        //}
     }
 }
